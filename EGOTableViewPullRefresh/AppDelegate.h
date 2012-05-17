6c118d5bcae08147e8ea24dc7c6eb82dbd27f1e0
曹 世博 <caoshibo@localhost.(none)>
2012-05-17 14:26:14 +0800

    Initial Commit

---
+++ EGOTableViewPullRefresh/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EGOTableViewPullRefresh
+//
+//  Created by 世博 曹 on 12-5-17.
+//  Copyright (c) 2012年 华夏未来. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EGOTableViewPullRefresh/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EGOTableViewPullRefresh
+//
+//  Created by 世博 曹 on 12-5-17.
+//  Copyright (c) 2012年 华夏未来. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
